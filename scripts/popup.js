24dc77669d7ad2d7f9fe3a7c368156d26d987f63
Haloroute <quangtuyensctn@gmail.com>
2024-03-01 13:41:02 +0700

    Restore old codes

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,32 +1,29 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-    var showUrlButton = document.getElementById('showUrlButton');
-    var reloadUrlButton = document.getElementById('reloadButton');
-    var urlLabel = document.getElementById('urlLabel');
-    var currentUrlSpan = document.getElementById('currentUrl');
     var checkUrlLabel = document.getElementById('checkUrlLabel');
-    var formUrlSpan = document.getElementById('formUrl');
+    var checkUrlSpan = document.getElementById('checkUrlSpan');
+    var quizLabel = document.getElementById('quizLabel');
+    var quizSpan = document.getElementById('quizSpan');
+    var loadButton = document.getElementById('loadButton');
     
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var currentTab = tabs[0];
         if (currentTab.url.match(/https\:\/\/forms\.office\.com\/Pages\/ResponsePage/i)) {
-            formUrlSpan.textContent = "Microsoft Forms";
-            showUrlButton.disabled = false;
+            checkUrlSpan.textContent = "Trang web này là Microsoft Forms";
         } else {
-            formUrlSpan.textContent = "not Microsoft Forms";
-            showUrlButton.disabled = true;
+            checkUrlSpan.textContent = "Trang web này không phải là Microsoft Forms";
+            loadButton.disabled = true;
+            loadButton.style.background = '#d3d3d3';
+            loadButton.style.cursor = 'not-allowed';
+            checkUrlSpan.style.color = '#d3d3d3';
         };
     });
 
-    showUrlButton.addEventListener('click', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            var currentTab = tabs[0];
-            var url = currentTab.url;
-            currentUrlSpan.textContent = url;
-            urlLabel.style.display = 'block';
-        });
-    });
-
-    reloadUrlButton.addEventListener('click', function () {
-        chrome.tabs.reload();
-    });
+    //showUrlButton.addEventListener('click', function () {
+    //    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //        var currentTab = tabs[0];
+    //        var url = currentTab.url;
+    //        currentUrlSpan.textContent = url;
+    //        urlLabel.style.display = 'block';
+    //    });
+    //});
 });
