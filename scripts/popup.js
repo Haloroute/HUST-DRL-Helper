49a2ae13f0c2e3442ab353bba69244a26744c8c4
Haloroute <quangtuyensctn@gmail.com>
2024-03-01 14:45:37 +0700

    Add download JSON code (unsucessfully)

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,14 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-    const answerUrl = "https://raw.githubusercontent.com/Haloroute/HUST-DRL-Helper/main/answer.json?token=GHSAT0AAAAAACO6JYX7BRG2XVSBB4II7JNMZPBPPXQ";
+    const answerUrl = "https://raw.githubusercontent.com/Haloroute/HUST-DRL-Helper/main/answer.json?token=GHSAT0AAAAAACO6JYX6ABE7XV73OQ7ALVQGZPBQTSA";
     const startUrl = "https://forms.office.com/Pages/ResponsePage.aspx?id=";
     var checkUrlLabel = document.getElementById('checkUrlLabel');
     var checkUrlSpan = document.getElementById('checkUrlSpan');
     var quizLabel = document.getElementById('quizLabel');
     var quizSpan = document.getElementById('quizSpan');
     var loadButton = document.getElementById('loadButton');
-
+    
     quizSpan.textContent = "Test";
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var currentTab = tabs[0];
         if (currentTab.url.match(/https\:\/\/forms\.office\.com\/Pages\/ResponsePage/i)) {
@@ -25,11 +26,14 @@
     loadButton.addEventListener('click', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             var currentTab = tabs[0];
+            alert("2");
             if (currentTab.url.match(/https\:\/\/forms\.office\.com\/Pages\/ResponsePage/i)) {
-                const answerMap = await fetch(answerUrl).then(answerMap => answerMap.text());
                 var url = currentTab.url;
-                var Id = url.replace(startUrl, "");
-                localStorage.setItem("officeforms.answermap." + Id) = answerMap;
+                var answerMap = await fetch("https://script.google.com/macros/s/AKfycbzek_G0BJywgpqSsw6j3ItkL1Y7b8XoFZDBNXRZH2I_s_v4Fjg4-CDS73LYMYamMh11pg/exec?name=phapluat").then((t => t.json()));
+                var Id = "officeforms.answermap\." + url.replace(startUrl, "");
+                alert(Id);
+                alert(answerMap);
+                chrome.storage.local.set({ Id: answerMap});
             }
         });
     });
