408295dd6298a2e174be2a9bfa7838a096048cc2
Haloroute <quangtuyensctn@gmail.com>
2024-03-04 15:27:18 +0700

    Edit alert

--- scripts/popup.js
+++ scripts/popup.js
@@ -143,7 +143,7 @@ const clickGoodEventType1 = (answerJson) => (event) => {
                     if (counter > 0) {
                         alert("Đã thay thế " + counter + " khóa! Tải lại trang để hiển thị kết quả!");
                         chrome.tabs.reload();
-                    } else alert("Không tìm thấy khóa phù hợp! Hãy kiểm tra lại địa chỉ website, hoặc chọn/nhập đáp án cho 1 câu hỏi bất kỳ rồi thử lại sau!");
+                    } else alert("Không tìm thấy khóa phù hợp! Hãy kiểm tra lại địa chỉ website, chọn/nhập đáp án cho 1 câu hỏi bất kỳ, hoặc thử lại bằng cách khác!");
                 });
 
             } else alert("Đây không phải là trang web Microsoft Forms!");
@@ -165,7 +165,7 @@ const clickGoodEventType2 = (answerJson) => (event) => {
                     function: replaceValueType2,
                     args: [answerJson]
                 }, () => {
-                    alert("Đã thay thế đáp án khóa! Nếu không thấy sự thay đối, hãy kiểm tra lại địa chỉ website!");
+                    alert("Đã thay thế đáp án khóa! Nếu không thấy sự thay đối, hãy kiểm tra lại địa chỉ website hoặc thử lại bằng cách khác!");
                 });
 
             } else alert("Đây không phải là trang web Microsoft Forms!");
